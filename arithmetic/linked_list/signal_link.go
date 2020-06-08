00c44af487fde0c5669022842d917355ac5002ac
wanli.cheng <wanli.cheng@enmotech.com>
2020-06-08 15:05:58 +0800

    alter linkedList

---
+++ arithmetic/linked_list/signal_link.go
@@ -0,0 +1,45 @@
+package mian
+
+// 1. 反转位置m-n的单链表，请使用一趟扫描完成反转
+// 例： 1->2->3->4->5  reverse: 1->4->3->2->5
+
+// ListNode ListNode
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseElement(head *ListNode, m, n int) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	he := &ListNode{}
+	he.Next = head
+	cur := he
+
+	for i := 0; i < m-1; i++ {
+		cur = cur.Next
+	}
+
+	h := cur.Next
+	t := h.Next
+
+	var tail *ListNode
+	for i := 0; i < n-m; i++ {
+		temp := t.Next
+		t.Next = h
+		if tail == nil {
+			tail = h
+		}
+
+		h = t
+		t = temp
+	}
+
+	cur.Next = h
+	if tail != nil {
+		tail.Next = t
+	}
+	return he.Next
+}
---
+++ arithmetic/linked_list/signal_link_test.go
@@ -0,0 +1,31 @@
+package mian
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_reverseElement(t *testing.T) {
+	tests := []struct {
+		name   string
+		Linked *ListNode
+	}{
+		// TODO: Add test cases.
+		struct {
+			name   string
+			Linked *ListNode
+		}{
+			"reverse list len is 5",
+			&ListNode{1,
+				&ListNode{2,
+					&ListNode{3,
+						&ListNode{4,
+							&ListNode{5, nil}}}}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := reverseElement(tt.Linked, 2, 4)
+			fmt.Printf("got is: %v\n", *got)
+		})
+	}
+}
